c5078155500f3ad29f7b616004272303604fa479
ngmatos <nunobarros_@hotmail.com>
2018-02-10 14:31:50 +0000

    subscriber nows shows messages in utf-8

--- mqtt/mqtt_subscriber.py
+++ mqtt/mqtt_subscriber.py
@@ -1,17 +1,16 @@
 import paho.mqtt.client as mqtt
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected with code" + str(rc))
+    print("Connected with code " + str(rc))
 
-    client.subscribe("test", 1)
+    client.subscribe("test")
 
 def on_message(client, userdata, msg):
-    print(msg.topic + " " + str(msg.payload))
+    print(msg.payload.decode('utf-8', 'ignore'))
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
-
 client.connect("localhost", 1883, 60)
 
 client.loop_forever()
