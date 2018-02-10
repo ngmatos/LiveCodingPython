3ad0d47ac26d76f0e13a26748b0cea94c684765e
ngmatos <nunobarros_@hotmail.com>
2018-02-10 14:59:26 +0000

    event handler now does not read hidden files (.*)

--- event_handler.py
+++ event_handler.py
@@ -1,3 +1,4 @@
+import os
 import sys
 sys.path.append('./file_readers')
 sys.path.append('./mqtt')
@@ -5,15 +6,18 @@ from file_reader import FileReader
 
 from datetime import datetime
 from watchdog.events import FileSystemEventHandler
+#from watchdog.events import PatternMatchingEventHandler
 from mqtt_publisher import Publisher
 
 class EventHandler(FileSystemEventHandler):
-        
+    
     def on_modified(self, event):
         publisher = Publisher()
-        reader = FileReader(event.src_path)
+        reader = FileReader(event.src_path)        
 
-        if not event.is_directory:
+        if not event.is_directory and not str(os.path.basename(event.src_path)).startswith('.'):
+
+            print('passei')
 
             is_dir = 'directory' if event.is_directory else 'file'
             log = str(datetime.now()) + " Modified " + is_dir + " -> " + event.src_path
